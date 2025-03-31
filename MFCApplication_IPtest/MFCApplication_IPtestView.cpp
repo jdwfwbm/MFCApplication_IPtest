b0f51cba73cbff339f85c5666a7aece2fb9708a1
jdwfwbm <ted0426@aver.com>
2025-03-31 16:59:34 +0900

    초기 커밋.

---
+++ MFCApplication_IPtest/MFCApplication_IPtestView.cpp
@@ -0,0 +1,128 @@
+﻿
+// MFCApplication_IPtestView.cpp: CMFCApplicationIPtestView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "MFCApplication_IPtest.h"
+#endif
+
+#include "MFCApplication_IPtestDoc.h"
+#include "MFCApplication_IPtestView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplicationIPtestView
+
+IMPLEMENT_DYNCREATE(CMFCApplicationIPtestView, CView)
+
+BEGIN_MESSAGE_MAP(CMFCApplicationIPtestView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CMFCApplicationIPtestView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CMFCApplicationIPtestView 생성/소멸
+
+CMFCApplicationIPtestView::CMFCApplicationIPtestView() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CMFCApplicationIPtestView::~CMFCApplicationIPtestView()
+{
+}
+
+BOOL CMFCApplicationIPtestView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFCApplicationIPtestView 그리기
+
+void CMFCApplicationIPtestView::OnDraw(CDC* /*pDC*/)
+{
+	CMFCApplicationIPtestDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+
+// CMFCApplicationIPtestView 인쇄
+
+
+void CMFCApplicationIPtestView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CMFCApplicationIPtestView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CMFCApplicationIPtestView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CMFCApplicationIPtestView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+void CMFCApplicationIPtestView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CMFCApplicationIPtestView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CMFCApplicationIPtestView 진단
+
+#ifdef _DEBUG
+void CMFCApplicationIPtestView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCApplicationIPtestView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCApplicationIPtestDoc* CMFCApplicationIPtestView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCApplicationIPtestDoc)));
+	return (CMFCApplicationIPtestDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCApplicationIPtestView 메시지 처리기
